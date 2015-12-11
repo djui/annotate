988ac45dac42e75afb9b182c3c97f56cd459da45
Uwe Dauernheim <uwe@dauernheim.net>
2015-12-11 23:30:28 +0000

    Split on `\r`-only lines as well
    Some programs use progress bar on a single line which overrides the prefix.

--- annotate.go
+++ annotate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -28,10 +29,43 @@ func pipe(w io.Writer, f func(*readWriter)) io.Writer {
 // annotate each line read from "r" with prefix and write to "w".
 func annotate(rw *readWriter, prefix func() string) error {
 	s := bufio.NewScanner(rw)
+	s.Split(scanLines)
 	for s.Scan() {
 		printAnnotated(rw, prefix, s.Text())
 	}
 	return s.Err()
+}
+
+// ScanLines is a split function for a Scanner that returns each line of text,
+// stripped of any trailing end-of-line marker. The returned line may be
+// empty. The end-of-line marker is either one carriage return not followed by a
+// newline or one optional carriage return followed by one mandatory newline or
+// one carriage return. In regular expression notation, it is `\r[^\n]|\r?\n`.
+// The last non-empty line of input will be returned even if it has no newline.
+func scanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	r := bytes.IndexByte(data, '\r')
+	n := bytes.IndexByte(data, '\n')
+	// We have a full \r\n-terminated line.
+	if r >= 0 && n == r+1 {
+		return r + 2, data[0:r], nil
+	}
+	// We have a full \r-terminated line.
+	if r >= 0 && (n < 0 || r < n) {
+		return r + 1, data[0:r], nil
+	}
+	// We have a full \n-terminated line.
+	if n >= 0 && (r < 0 || n < r) {
+		return n + 1, data[0:n], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
 
 func printArguments(args []string, prefix func() string) {
